2253a5e4ec5f51855509189034a3702c52ef1e04
Taiyu <64182347+TaiYu-Su@users.noreply.github.com>
2020-06-07 11:52:24 +0800

    Delete Example_FileIO.py

--- How to read csv and json with class and enumerate/Example_FileIO.py
+++
@@ -1,9 +0,0 @@
-# 使用"w"可以寫入資料
-# f = open("data.txt", "w", encoding="UTF-8")
-# user_input = input("請輸入文字 : ")
-# f.write(user_input)
-
-# with 可在使用完畢後自動關閉程式
-with open("data.txt", "a", encoding="UTF-8") as file:
-    user_input = input("請輸入文字 : ")
-    file.write(user_input + "\n")
